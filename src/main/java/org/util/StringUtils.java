094333ec63b067ac7307b1b74c0edd6114151697
dimaperehuda <mabrodi7@gmail.com>
2025-07-20 16:11:26 +0300

    remove util

--- src/main/java/org/util/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package org.util;
-
-public class StringUtils {
-    public static String escapeHtml(String s) {
-        if (s == null) return "";
-        return s.replace("&", "&amp;").replace("<", "&lt;").replace(">", "&gt;")
-                .replace("\"", "&quot;").replace("'", "&#39;");
-    }
-}
